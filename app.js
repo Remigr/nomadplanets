5f11306b331071712c7043cbf20f3d3ca700562c
Remigr <remiclaude@free.fr>
2015-02-01 00:54:45 +0200

    Take away NODE_PATH

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 var fs = require('fs');
 var express = require('express');
 var mongoose = require('mongoose');
-var config = require('config');
+var config = require('./config/config');
 
 var app = express();
 var port = process.env.PORT || 8080;
